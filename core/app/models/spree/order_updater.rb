925a9a070305586664f8bee68e21cebdf166f49b
Magic <adrian.toczydlowski@antdesk.com>
2013-12-30 13:58:06 +0100

    ss

--- core/app/models/spree/order_updater.rb
+++ core/app/models/spree/order_updater.rb
@@ -100,14 +100,9 @@ module Spree
     #
     # The +payment_state+ value helps with reporting, etc. since it provides a quick and easy way to locate Orders needing attention.
     def update_payment_state
-      'testestest !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'.to_yaml
-      round_money(order.total.to_i).to_yaml
-      round_money(order.total.to_i).inspect
-      debug round_money(order.total.to_i)
-      raise round_money(order.total.to_i).to_s
 
       #line_item are empty when user empties cart
-      if line_items.empty? || round_money(order.payment_total) < round_money(order.total.to_i)
+      if line_items.empty? || order.payment_total < order.total.to_i
         if payments.present? && payments.last.state == 'failed'
           order.payment_state = 'failed'
         else
