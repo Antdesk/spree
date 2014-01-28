1180d788c58aaee070fafd322a4478271ca7b57a
Magic <adrian.toczydlowski@antdesk.com>
2014-01-28 11:54:27 +0100

    asd

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -218,8 +218,8 @@ module Spree
     def line_item_adjustment_totals
       Hash[self.line_item_adjustments.eligible.group_by(&:label).map do |label, adjustments|
         total = adjustments.sum(&:amount)
-        temp = total + " " + currency
-        [label, temp]
+
+        [label, total, currency]
       end]
     end
 
